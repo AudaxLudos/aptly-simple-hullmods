22594b6e7597099fc2f2a08b4f448b37f64a3375
unknown <james.tarrobal.777@gmail.com>
2022-05-02 20:30:04 +0800

    Added Graviton Attunement Drive Hullmod

---
+++ src/data/hullmods/ASH_GravitonAttunementDrive.java
@@ -0,0 +1,36 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+
+public class ASH_GravitonAttunementDrive extends BaseHullMod {
+    public final float FLEET_BURN_MODIFIER = 3f;
+    public final float SENSOR_PROFILE_MODIFIER = 3f;
+    public final float SUPPLIES_PER_MONTH_MODIFIER = 3f;
+
+    public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
+        stats.getDynamic().getMod("fleet_burn_bonus").modifyFlat(id, FLEET_BURN_MODIFIER);
+        stats.getSensorProfile().modifyMult(id, SENSOR_PROFILE_MODIFIER);
+        stats.getSuppliesPerMonth().modifyMult(id, SUPPLIES_PER_MONTH_MODIFIER);
+    }
+
+    public String getUnapplicableReason(ShipAPI ship) {
+        if (ship.getHullSize() != HullSize.CAPITAL_SHIP)
+            return "Ship must be a Capital Ship";
+        return null;
+    }
+
+    public boolean isApplicableToShip(ShipAPI ship) {
+        return ship.getHullSize() == HullSize.CAPITAL_SHIP;
+    }
+
+    public String getDescriptionParam(int index, HullSize hullSize) {
+        if (index == 0)
+            return (int) FLEET_BURN_MODIFIER + "";
+        if (index == 1)
+            return (int) 3 + "";
+        return null;
+    }
+}
