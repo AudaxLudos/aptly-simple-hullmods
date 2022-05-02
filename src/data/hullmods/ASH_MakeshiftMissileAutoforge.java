b00faae820bc333d1089a93ccc006806a9e7b538
unknown <james.tarrobal.777@gmail.com>
2022-05-03 02:35:10 +0800

    Added Makeshift Missile Autoforge

--- src/data/hullmods/ASH_MakeshiftMissileAutoforge.java
+++ src/data/hullmods/ASH_MakeshiftMissileAutoforge.java
@@ -3,26 +3,16 @@ package data.hullmods;
 import java.util.List;
 
 import com.fs.starfarer.api.combat.BaseHullMod;
-import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.WeaponAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 import com.fs.starfarer.api.combat.WeaponAPI.WeaponType;
 
-import data.ASH_Utils;
-
-public class ASH_TestHullmod extends BaseHullMod {
-    public final float MODIFIER = 15f;
-
-    @Override
-    public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
-        stats.getMissileRoFMult().modifyMult(id, MODIFIER);
-    }
-
+public class ASH_MakeshiftMissileAutoforge extends BaseHullMod {
     @Override
     public String getDescriptionParam(int index, HullSize hullSize) {
         if (index == 0)
-            return (int) MODIFIER + "";
+            return (int) 0 + "";
         return null;
     }
 
@@ -31,13 +21,11 @@ public class ASH_TestHullmod extends BaseHullMod {
         if (!ship.isAlive())
             return;
 
-        MutableShipStatsAPI stats = ship.getMutableStats();
-
         List<WeaponAPI> weapons = ship.getAllWeapons();
         for (WeaponAPI weapon : weapons) {
             if (weapon.getType() == WeaponType.MISSILE && weapon.usesAmmo() && weapon.getAmmo() <= 0f && weapon.getAmmoPerSecond() == 0f) {
                 float ammoReloadSize = (float) Math.ceil(weapon.getMaxAmmo() * 0.1f);
-                float ammoPerSecond = 0.01666666666666666666666666666667f * ammoReloadSize;
+                float ammoPerSecond = 0.02f * ammoReloadSize;
                 weapon.getAmmoTracker().setReloadSize(ammoReloadSize);
                 weapon.getAmmoTracker().setAmmoPerSecond(ammoPerSecond);
             }
