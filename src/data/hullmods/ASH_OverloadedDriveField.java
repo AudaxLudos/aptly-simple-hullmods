0ee78c2db90defbf4504fd56c4bc3116d8159de2
unknown <james.tarrobal.777@gmail.com>
2022-05-13 05:10:32 +0800

    Added Overloaded Drive Field Hullmod

---
+++ src/data/hullmods/ASH_OverloadedDriveField.java
@@ -0,0 +1,34 @@
+package data.hullmods;
+
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+
+public class ASH_OverloadedDriveField extends BaseHullMod {
+    public static final float MAX_BURN_LEVEL_MODIFIER = 4f;
+    public static final float FUEL_USE_MODIFIER = 2f;
+    public static final float SENSOR_PROFILE_MODIFIER = 100f;
+    public static final float SENSOR_STRENGTH_MODIFIER = 50f;
+
+    @Override
+    public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
+        stats.getMaxBurnLevel().modifyFlat(id, MAX_BURN_LEVEL_MODIFIER);
+        stats.getFuelUseMod().modifyMult(id, FUEL_USE_MODIFIER);
+        stats.getSensorProfile().modifyPercent(id, SENSOR_PROFILE_MODIFIER);
+        stats.getSensorStrength().modifyPercent(id, -SENSOR_STRENGTH_MODIFIER);
+    }
+
+    @Override
+    public String getDescriptionParam(int index, HullSize hullSize) {
+        if (index == 0)
+            return Math.round(MAX_BURN_LEVEL_MODIFIER) + "";
+        if (index == 1)
+            return Math.round(FUEL_USE_MODIFIER) + "";
+        if (index == 2)
+            return Math.round(SENSOR_PROFILE_MODIFIER) + "%";
+        if (index == 3)
+            return Math.round(SENSOR_STRENGTH_MODIFIER) + "%";
+        return null;
+    }
+}
