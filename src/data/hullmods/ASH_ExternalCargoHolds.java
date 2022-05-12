369fd6b58b8e64b9be4e707ac0aa7dc63f43dfee
unknown <james.tarrobal.777@gmail.com>
2022-05-13 04:08:48 +0800

    Added External Cargo Holds Hullmod

---
+++ src/data/hullmods/ASH_ExternalCargoHolds.java
@@ -0,0 +1,56 @@
+package data.hullmods;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.fs.starfarer.api.combat.BaseHullMod;
+import com.fs.starfarer.api.combat.MutableShipStatsAPI;
+import com.fs.starfarer.api.combat.ShipAPI;
+import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.impl.campaign.ids.HullMods;
+
+public class ASH_ExternalCargoHolds extends BaseHullMod {
+    public static final float SHIP_STATS_MODIFIER = 25f;
+    private static Map CARGO_MODIFIER = new HashMap();
+    static {
+        CARGO_MODIFIER.put(HullSize.FRIGATE, 250f);
+        CARGO_MODIFIER.put(HullSize.DESTROYER, 500f);
+        CARGO_MODIFIER.put(HullSize.CRUISER, 750f);
+        CARGO_MODIFIER.put(HullSize.CAPITAL_SHIP, 1000f);
+    }
+
+    @Override
+    public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
+        stats.getCargoMod().modifyFlat(id, (Float) CARGO_MODIFIER.get(hullSize));
+        stats.getMaxSpeed().modifyPercent(id, -SHIP_STATS_MODIFIER);
+        stats.getMaxTurnRate().modifyPercent(id, -SHIP_STATS_MODIFIER);
+        stats.getFluxDissipation().modifyPercent(id, -SHIP_STATS_MODIFIER);
+    }
+
+    @Override
+    public String getUnapplicableReason(ShipAPI ship) {
+        if (ship == null || (ship.getVariant().hasHullMod(HullMods.CIVGRADE) && !ship.getVariant().hasHullMod(HullMods.MILITARIZED_SUBSYSTEMS)))
+            return "Cannot be installed on a civilian ship without Militarized Subsystems";
+        return null;
+    }
+
+    @Override
+    public boolean isApplicableToShip(ShipAPI ship) {
+        return ship != null && (!ship.getVariant().hasHullMod(HullMods.CIVGRADE) || ship.getVariant().hasHullMod(HullMods.MILITARIZED_SUBSYSTEMS));
+    }
+
+    @Override
+    public String getDescriptionParam(int index, HullSize hullSize) {
+        if (index == 0)
+            return Math.round(((Float) CARGO_MODIFIER.get(HullSize.FRIGATE)).intValue()) + "";
+        if (index == 1)
+            return Math.round(((Float) CARGO_MODIFIER.get(HullSize.FRIGATE)).intValue()) + "";
+        if (index == 2)
+            return Math.round(((Float) CARGO_MODIFIER.get(HullSize.FRIGATE)).intValue()) + "";
+        if (index == 3)
+            return Math.round(((Float) CARGO_MODIFIER.get(HullSize.FRIGATE)).intValue()) + "";
+        if (index == 4)
+            return Math.round(SHIP_STATS_MODIFIER) + "%";
+        return null;
+    }
+}
--- src/data/hullmods/ASH_TestHullmod.java
+++ src/data/hullmods/ASH_TestHullmod.java
@@ -1,33 +1,49 @@
 package data.hullmods;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
-import com.fs.starfarer.api.impl.campaign.ids.Stats;
 
 public class ASH_TestHullmod extends BaseHullMod {
-    public static final float MODIFIER = 25f;
+    public static final float MAX_BURN_LEVEL_MODIFIER = 1f;
+    public static final float MAX_SPEED_MODIFIER = 20f;
+    public static final float MAX_TURN_RATE_MODIFIER = 20f;
+    private static Map CARGO_MODIFIER = new HashMap();
+    static {
+        CARGO_MODIFIER.put(HullSize.FRIGATE, 250f);
+        CARGO_MODIFIER.put(HullSize.DESTROYER, 500f);
+        CARGO_MODIFIER.put(HullSize.CRUISER, 750f);
+        CARGO_MODIFIER.put(HullSize.CAPITAL_SHIP, 1000f);
+    }
 
     @Override
     public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
-        stats.getMaxCombatReadiness().modifyFlat(id, 15f * 0.01f, "Hullmod");
-        stats.getPeakCRDuration().modifyFlat(id, 60f);
-        stats.getCRLossPerSecondPercent().modifyPercent(id, 50f);
+        stats.getCargoMod().modifyFlat(id, (Float) CARGO_MODIFIER.get(hullSize));
+        stats.getMaxBurnLevel().modifyFlat(id, -MAX_BURN_LEVEL_MODIFIER);
+        stats.getMaxSpeed().modifyPercent(id, -MAX_SPEED_MODIFIER);
+        stats.getMaxTurnRate().modifyPercent(id, -MAX_TURN_RATE_MODIFIER);
     }
 
     @Override
     public String getDescriptionParam(int index, HullSize hullSize) {
         if (index == 0)
-            return Math.round(MODIFIER) + "%";
+            return Math.round(((Float) CARGO_MODIFIER.get(HullSize.FRIGATE)).intValue()) + "%";
+        if (index == 1)
+            return Math.round(((Float) CARGO_MODIFIER.get(HullSize.FRIGATE)).intValue()) + "%";
+        if (index == 2)
+            return Math.round(((Float) CARGO_MODIFIER.get(HullSize.FRIGATE)).intValue()) + "%";
+        if (index == 3)
+            return Math.round(((Float) CARGO_MODIFIER.get(HullSize.FRIGATE)).intValue()) + "%";
+        if (index == 4)
+            return Math.round(MAX_BURN_LEVEL_MODIFIER) + "%";
+        if (index == 5)
+            return Math.round(MAX_SPEED_MODIFIER) + "%";
+        if (index == 6)
+            return Math.round(MAX_TURN_RATE_MODIFIER) + "%";
         return null;
-    }
-
-    @Override
-    public void advanceInCombat(ShipAPI ship, float amount) {
-        if (!ship.isAlive())
-            return;
-
-        MutableShipStatsAPI stats = ship.getMutableStats();
     }
 }
