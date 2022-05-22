18bfa2ecb5f8f6098ef4cca984ea6f5cd60f4462
unknown <james.tarrobal.777@gmail.com>
2022-05-22 16:48:33 +0800

    Updated Targeting Receiver Hullmod

--- src/data/hullmods/ASH_TargetingTransceiver.java
+++ src/data/hullmods/ASH_TargetingTransceiver.java
@@ -8,28 +8,21 @@ import com.fs.starfarer.api.impl.campaign.ids.HullMods;
 
 import org.lazywizard.lazylib.combat.AIUtils;
 
-public class ASH_TargetingReceiver extends BaseHullMod {
+public class ASH_TargetingTransceiver extends BaseHullMod {
     public static final float WEAPON_RANGE_MODIFIER = 10f;
-    public static final float MAX_RANGE_THRESHOLD = 500f;
-
-    @Override
-    public String getUnapplicableReason(ShipAPI ship) {
-        if (ship.getHullSize() == HullSize.CRUISER || ship.getHullSize() == HullSize.CAPITAL_SHIP)
-            return "Cannot be installed on Cruisers or Capital ships";
-        return null;
-    }
-
-    @Override
-    public boolean isApplicableToShip(ShipAPI ship) {
-        return ship.getHullSize() != HullSize.CAPITAL_SHIP && ship.getHullSize() != HullSize.CRUISER;
-    }
+    public static final float AUTOFIRE_AIM_ACCURACY_MODIFIER = 40f;
+    public static final float MAX_RANGE_THRESHOLD = 750f;
 
     @Override
     public String getDescriptionParam(int index, HullSize hullSize) {
         if (index == 0)
             return Math.round(WEAPON_RANGE_MODIFIER) + "%";
         if (index == 1)
-            return Math.round(MAX_RANGE_THRESHOLD) + "";
+            return Math.round(AUTOFIRE_AIM_ACCURACY_MODIFIER) + "%";
+        if (index == 2)
+            return Math.round(MAX_RANGE_THRESHOLD + 250f) + "su";
+        if (index == 3)
+            return "Targeting Core / Unit";
         return null;
     }
 
@@ -40,6 +33,7 @@ public class ASH_TargetingReceiver extends BaseHullMod {
 
         MutableShipStatsAPI stats = ship.getMutableStats();
         float computedRangeBonus = 0f;
+        float computedAutofireAimAccuracy = 0f;
 
         for (ShipAPI ally : AIUtils.getNearbyAllies(ship, MAX_RANGE_THRESHOLD)) {
             if (ally.isFrigate() || ally.isDestroyer() || ally.isDrone() || ally.isFighter())
@@ -47,11 +41,16 @@ public class ASH_TargetingReceiver extends BaseHullMod {
 
             if (ally.getVariant().hasHullMod(HullMods.DEDICATED_TARGETING_CORE) ||
                     ally.getVariant().hasHullMod(HullMods.INTEGRATED_TARGETING_UNIT) ||
-                    ally.getVariant().hasHullMod(HullMods.ADVANCED_TARGETING_CORE))
+                    ally.getVariant().hasHullMod(HullMods.ADVANCED_TARGETING_CORE)) {
                 computedRangeBonus = WEAPON_RANGE_MODIFIER;
+                computedAutofireAimAccuracy = AUTOFIRE_AIM_ACCURACY_MODIFIER;
+            }
         }
 
-        stats.getEnergyWeaponRangeBonus().modifyPercent(spec.getId(), computedRangeBonus);
-        stats.getBallisticWeaponRangeBonus().modifyPercent(spec.getId(), computedRangeBonus);
+        if (ship.getHullSize() == HullSize.FRIGATE || ship.getHullSize() == HullSize.DESTROYER) {
+            stats.getEnergyWeaponRangeBonus().modifyPercent(spec.getId(), computedRangeBonus);
+            stats.getBallisticWeaponRangeBonus().modifyPercent(spec.getId(), computedRangeBonus);
+        } else
+            stats.getAutofireAimAccuracy().modifyPercent(spec.getId(), computedAutofireAimAccuracy);
     }
 }
