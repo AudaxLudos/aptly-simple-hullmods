68233bd447a1a67f2d6b165d91887677b8642357
unknown <james.tarrobal.777@gmail.com>
2022-08-28 11:40:29 +0800

    Balanced Enhanced Autoloaders

--- src/data/hullmods/ASH_EnhancedAutoloaders.java
+++ src/data/hullmods/ASH_EnhancedAutoloaders.java
@@ -7,22 +7,18 @@ import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 
 public class ASH_EnhancedAutoloaders extends BaseHullMod {
     public static final float BALLISTIC_STATS_MODIFIER = 100f;
-    public static final float BALLISTIC_DAMAGE_MODIFIER = 25f;
 
     @Override
     public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
         stats.getBallisticRoFMult().modifyPercent(id, BALLISTIC_STATS_MODIFIER);
         stats.getBallisticAmmoRegenMult().modifyPercent(id, BALLISTIC_STATS_MODIFIER);
         stats.getBallisticAmmoBonus().modifyPercent(id, BALLISTIC_STATS_MODIFIER);
-        stats.getBallisticWeaponDamageMult().modifyPercent(id, -BALLISTIC_DAMAGE_MODIFIER);
     }
 
     @Override
     public String getDescriptionParam(int index, HullSize hullSize) {
         if (index == 0)
             return Math.round(BALLISTIC_STATS_MODIFIER) + "%";
-        if (index == 1)
-            return Math.round(BALLISTIC_DAMAGE_MODIFIER) + "%";
         return null;
     }
 }
