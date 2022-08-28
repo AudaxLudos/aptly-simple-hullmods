bc82b93ed709432a3c407897f8821f2f51f64bc2
unknown <james.tarrobal.777@gmail.com>
2022-08-28 23:44:50 +0800

    Updated Excessive Venting Protocol to Venting Overdrive

--- src/data/hullmods/ASH_VentingOverdrive.java
+++ src/data/hullmods/ASH_VentingOverdrive.java
@@ -11,11 +11,9 @@ import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 
-public class ASH_ExcessiveVentingProtocol extends BaseHullMod {
+public class ASH_VentingOverdrive extends BaseHullMod {
     public static final float MAX_SPEED_MODIFIER = 2f;
     public static final float MAX_TURN_RATE_MODIFIER = 2f;
-    public static final float ACCELERATION_MODIFIER = 2f;
-    public static final float TURN_ACCELERATION_MODIFIER = 2f;
     public static final float VENT_RATE_MODIFIER = 2f;
 
     @Override
@@ -27,20 +25,19 @@ public class ASH_ExcessiveVentingProtocol extends BaseHullMod {
         CombatEngineAPI engine = Global.getCombatEngine();
 
         if (ship.getHullLevel() >= 0.2f) {
-            stats.getVentRateMult().modifyMult(spec.getId(), 2f);
+            stats.getVentRateMult().modifyMult(spec.getId(), VENT_RATE_MODIFIER);
 
             if (ship.getFluxTracker().isVenting()) {
-                stats.getMaxSpeed().modifyMult(spec.getId(), 2f);
-                stats.getMaxTurnRate().modifyMult(spec.getId(), 2f);
-                stats.getAcceleration().modifyMult(spec.getId(), 2f);
-                stats.getTurnAcceleration().modifyMult(spec.getId(), 2f);
+                stats.getMaxSpeed().modifyMult(spec.getId(), MAX_SPEED_MODIFIER);
+                stats.getMaxTurnRate().modifyMult(spec.getId(), MAX_TURN_RATE_MODIFIER);
 
                 Vector2f point = new Vector2f(ship.getLocation());
                 point.x += ship.getCollisionRadius() * ((float) Math.random() * 2.0F - 1.0F);
                 point.y += ship.getCollisionRadius() * ((float) Math.random() * 2.0F - 1.0F);
 
                 if (CollisionUtils.isPointWithinBounds(point, ship))
-                    engine.applyDamage(ship, point, amount * ship.getFluxTracker().getCurrFlux(), DamageType.OTHER, 0f, true, false, null);
+                    engine.applyDamage(ship, point, amount * ship.getFluxTracker().getCurrFlux(), DamageType.OTHER, 0f,
+                            true, false, null);
             } else {
                 stats.getMaxSpeed().unmodify();
                 stats.getMaxTurnRate().unmodify();
@@ -55,9 +52,11 @@ public class ASH_ExcessiveVentingProtocol extends BaseHullMod {
     @Override
     public String getDescriptionParam(int index, HullSize hullSize) {
         if (index == 0)
-            return Math.round(MAX_SPEED_MODIFIER) + "";
-        if (index == 3)
             return Math.round(VENT_RATE_MODIFIER) + "";
+        if (index == 1)
+            return Math.round(MAX_SPEED_MODIFIER) + "";
+        if (index == 2)
+            return "venting hurts the ship and the higher the flux levels the greater the damage received";
         return null;
     }
 }
