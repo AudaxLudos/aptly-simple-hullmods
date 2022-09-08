8b16dea2f642adfc2633f5daea56031994726b07
unknown <james.tarrobal.777@gmail.com>
2022-09-08 21:10:06 +0800

    MarineTF - removed maximum marines trainable

--- src/data/hullmods/ASH_MarineTrainingFacility.java
+++ src/data/hullmods/ASH_MarineTrainingFacility.java
@@ -16,15 +16,8 @@ public class ASH_MarineTrainingFacility extends BaseHullMod {
     static {
         MARINES_TO_GENERATE.put(HullSize.FRIGATE, 5);
         MARINES_TO_GENERATE.put(HullSize.DESTROYER, 10);
-        MARINES_TO_GENERATE.put(HullSize.CRUISER,20);
+        MARINES_TO_GENERATE.put(HullSize.CRUISER, 20);
         MARINES_TO_GENERATE.put(HullSize.CAPITAL_SHIP, 40);
-    }
-    private static Map<Object, Integer> MAX_MARINES_GENERATED = new HashMap<Object, Integer>();
-    static {
-        MAX_MARINES_GENERATED.put(HullSize.FRIGATE, 50);
-        MAX_MARINES_GENERATED.put(HullSize.DESTROYER, 100);
-        MAX_MARINES_GENERATED.put(HullSize.CRUISER, 200);
-        MAX_MARINES_GENERATED.put(HullSize.CAPITAL_SHIP, 250);
     }
     private long lastDay = Global.getSector().getClock().getTimestamp();
 
@@ -37,17 +30,10 @@ public class ASH_MarineTrainingFacility extends BaseHullMod {
         if (index == 2)
             return Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.CRUISER)).intValue()) + "";
         if (index == 3)
-            return Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.CAPITAL_SHIP)).intValue()) + " crew to marines";
+            return Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.CAPITAL_SHIP)).intValue())
+                    + " crew to marines";
         if (index == 4)
-            return "month";
-        if (index == 5)
-            return Math.round(((Integer) MAX_MARINES_GENERATED.get(HullSize.FRIGATE)).intValue()) + "";
-        if (index == 6)
-            return Math.round(((Integer) MAX_MARINES_GENERATED.get(HullSize.DESTROYER)).intValue()) + "";
-        if (index == 7)
-            return Math.round(((Integer) MAX_MARINES_GENERATED.get(HullSize.CRUISER)).intValue()) + "";
-        if (index == 8)
-            return Math.round(((Integer) MAX_MARINES_GENERATED.get(HullSize.CAPITAL_SHIP)).intValue()) + " marines";
+            return "week";
         return null;
     }
 
@@ -70,20 +56,18 @@ public class ASH_MarineTrainingFacility extends BaseHullMod {
             for (FleetMemberAPI fleetMember : member.getFleetData().getMembersListCopy()) {
                 minFleetCrew += fleetMember.getMinCrew();
 
-                if (fleetMember.getVariant().hasHullMod("ASH_MarineTrainingFacility")) {
+                if (fleetMember.getVariant().hasHullMod("ASH_MarineTrainingFacility"))
                     marinesGenerated += (Integer) MARINES_TO_GENERATE.get(fleetMember.getVariant().getHullSize());
-                    maxMarines += (Integer) MAX_MARINES_GENERATED.get(fleetMember.getVariant().getHullSize());
-                }
             }
 
-            if (member.getFleetData().getFleet().getCargo().getMarines() >= maxMarines || member.getFleetData().getFleet().getCargo().getCrew() <= minFleetCrew)
+            if (member.getFleetData().getFleet().getCargo().getMarines() >= maxMarines
+                    || member.getFleetData().getFleet().getCargo().getCrew() <= minFleetCrew)
                 return;
-            if (member.getFleetData().getFleet().getCargo().getMarines() + marinesGenerated >= maxMarines)
-                marinesGenerated = maxMarines - (int) member.getFleetData().getFleet().getCargo().getMarines();
             if (member.getFleetData().getFleet().getCargo().getCrew() - marinesGenerated <= minFleetCrew)
                 marinesGenerated = member.getFleetData().getFleet().getCargo().getCrew() - minFleetCrew;
-            if(marinesGenerated > 0)
-                Global.getSector().getCampaignUI().addMessage(marinesGenerated + " crew has been trained into marines.", Misc.getTextColor());
+            if (marinesGenerated > 0)
+                Global.getSector().getCampaignUI().addMessage(marinesGenerated + " crew has been trained into marines.",
+                        Misc.getTextColor());
 
             member.getFleetData().getFleet().getCargo().addMarines(marinesGenerated);
             member.getFleetData().getFleet().getCargo().removeCrew(marinesGenerated);
