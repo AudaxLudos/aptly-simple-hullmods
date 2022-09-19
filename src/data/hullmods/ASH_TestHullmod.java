c5a7f08962036b7dfb720bc17653ea1298da0fe2
unknown <james.tarrobal.777@gmail.com>
2022-09-19 18:49:46 +0800

    Updated hullmods tags

--- src/data/hullmods/ASH_TestHullmod.java
+++ src/data/hullmods/ASH_TestHullmod.java
@@ -1,12 +1,21 @@
 package data.hullmods;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 
 public class ASH_TestHullmod extends BaseHullMod {
-    public static final float TEST_MODIFIER = 100f;
+    private static Map<Object, Float> STATS_MODIFIER = new HashMap<Object, Float>();
+    static {
+        STATS_MODIFIER.put(HullSize.FRIGATE, 1f);
+        STATS_MODIFIER.put(HullSize.DESTROYER, 1f);
+        STATS_MODIFIER.put(HullSize.CRUISER, 2f);
+        STATS_MODIFIER.put(HullSize.CAPITAL_SHIP, 3f);
+    }
 
     @Override
     public void applyEffectsAfterShipCreation(ShipAPI ship, String id) {
@@ -14,6 +23,10 @@ public class ASH_TestHullmod extends BaseHullMod {
 
     @Override
     public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
+        float burnLevelBonus = 10f - stats.getMaxBurnLevel().getBaseValue();
+        stats.getMaxBurnLevel().modifyFlat(id, burnLevelBonus);
+        stats.getFuelUseMod().modifyMult(id, STATS_MODIFIER.get(hullSize));
+        stats.getSuppliesPerMonth().modifyMult(id, STATS_MODIFIER.get(hullSize));
     }
 
     @Override
@@ -25,13 +38,7 @@ public class ASH_TestHullmod extends BaseHullMod {
     @Override
     public String getDescriptionParam(int index, HullSize hullSize) {
         if (index == 0)
-            return Math.round(TEST_MODIFIER) + "%";
-        if (index == 1)
-            return Math.round(TEST_MODIFIER) + "%";
-        if (index == 2)
-            return Math.round(TEST_MODIFIER) + "%";
-        if (index == 3)
-            return Math.round(TEST_MODIFIER) + "%";
+            return "%";
         return null;
     }
 }
