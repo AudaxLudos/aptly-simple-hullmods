fd404c0aed440e14da13bd5094bbc03b99103241
unknown <james.tarrobal.777@gmail.com>
2022-10-14 13:48:43 +0800

    Update ASH_ReactiveSubsystems
    Add merc tag
    Update description
    Update OP cost to 4/8/12/20 from 6/8/12/24
    Update base value to 15000 from 8000
    Update cr loss per second to 25% from 50%

--- src/data/hullmods/ASH_ReactiveSubsystems.java
+++ src/data/hullmods/ASH_ReactiveSubsystems.java
@@ -1,30 +1,39 @@
 package data.hullmods;
 
+import java.awt.Color;
+
 import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.ui.Alignment;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
 
 public class ASH_ReactiveSubsystems extends BaseHullMod {
-    public static final float MAX_CR_MODIFIER = 15f;
+    public static final float MAX_CR_MODIFIER = 0.15f;
     public static final float PEAK_CR_DURATION_MODIFIER = 60f;
-    public static final float CR_LOSS_PER_SECOND_MODIFIER = 50f;
+    public static final float CR_LOSS_PER_SECOND_MULTIPLIER = 0.25f;
 
     @Override
     public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
-        stats.getMaxCombatReadiness().modifyFlat(id, MAX_CR_MODIFIER * 0.01f, "Reactive Subsystems Hullmod");
+        stats.getMaxCombatReadiness().modifyFlat(id, MAX_CR_MODIFIER, "Reactive Subsystems Hullmod");
         stats.getPeakCRDuration().modifyFlat(id, PEAK_CR_DURATION_MODIFIER);
-        stats.getCRLossPerSecondPercent().modifyPercent(id, CR_LOSS_PER_SECOND_MODIFIER);
+        stats.getCRLossPerSecondPercent().modifyMult(id, 1f + CR_LOSS_PER_SECOND_MULTIPLIER);
     }
 
     @Override
-    public String getDescriptionParam(int index, HullSize hullSize) {
-        if (index == 0)
-            return Math.round(MAX_CR_MODIFIER) + "%";
-        if (index == 1)
-            return Math.round(PEAK_CR_DURATION_MODIFIER) + " seconds";
-        if (index == 2)
-            return Math.round(CR_LOSS_PER_SECOND_MODIFIER) + "%";
-        return null;
+    public void addPostDescriptionSection(TooltipMakerAPI tooltip, HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec) {
+        float pad = 3f;
+        float opad = 10f;
+        Color good = Misc.getPositiveHighlightColor();
+        Color bad = Misc.getNegativeHighlightColor();
+
+        tooltip.addSectionHeading("Effects:", Alignment.MID, opad);
+        tooltip.setBulletedListMode(" - ");
+        tooltip.addPara("Increases the ship's max combat readiness by %s", opad, good, Math.round(MAX_CR_MODIFIER * 100f) + "%");
+        tooltip.addPara("Increases the ship's peak performance time by %s", pad, good, Math.round(PEAK_CR_DURATION_MODIFIER) + " seconds");
+        tooltip.addPara("Increases the rate at which combat readiness degrades by %s", pad, bad, Math.round(CR_LOSS_PER_SECOND_MULTIPLIER * 100f) + "%");
+        tooltip.setBulletedListMode(null);
     }
 }
