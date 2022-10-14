f5803bd51643c716acebdaa65d34417b36e730ac
unknown <james.tarrobal.777@gmail.com>
2022-10-15 01:03:48 +0800

    Update ASH_VentingOverdrive
    Add sprite
    Update description
    Update OP cost to 5/10/15/25 from 10/16/22/28
    Update calculation for damage recieved when venting
    Remove offensive tag

--- src/data/hullmods/ASH_VentingOverdrive.java
+++ src/data/hullmods/ASH_VentingOverdrive.java
@@ -1,5 +1,7 @@
 package data.hullmods;
 
+import java.awt.Color;
+
 import org.lazywizard.lazylib.CollisionUtils;
 import org.lwjgl.util.vector.Vector2f;
 
@@ -10,11 +12,12 @@ import com.fs.starfarer.api.combat.DamageType;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.ui.Alignment;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
 
 public class ASH_VentingOverdrive extends BaseHullMod {
-    public static final float MAX_SPEED_MODIFIER = 2f;
-    public static final float MAX_TURN_RATE_MODIFIER = 2f;
-    public static final float VENT_RATE_MODIFIER = 2f;
+    public static final float SHIP_STATS_MULTIPLIER = 1f;
 
     @Override
     public void advanceInCombat(ShipAPI ship, float amount) {
@@ -25,19 +28,20 @@ public class ASH_VentingOverdrive extends BaseHullMod {
         CombatEngineAPI engine = Global.getCombatEngine();
 
         if (ship.getHullLevel() >= 0.2f) {
-            stats.getVentRateMult().modifyMult(spec.getId(), VENT_RATE_MODIFIER);
+            stats.getVentRateMult().modifyMult(spec.getId(), 1f + SHIP_STATS_MULTIPLIER);
 
             if (ship.getFluxTracker().isVenting()) {
-                stats.getMaxSpeed().modifyMult(spec.getId(), MAX_SPEED_MODIFIER);
-                stats.getMaxTurnRate().modifyMult(spec.getId(), MAX_TURN_RATE_MODIFIER);
+                stats.getMaxSpeed().modifyMult(spec.getId(), 1f + SHIP_STATS_MULTIPLIER);
+                stats.getMaxTurnRate().modifyMult(spec.getId(), 1f + SHIP_STATS_MULTIPLIER);
 
                 Vector2f point = new Vector2f(ship.getLocation());
-                point.x += ship.getCollisionRadius() * ((float)Math.random() * 2.0F - 1.0F);
-                point.y += ship.getCollisionRadius() * ((float)Math.random() * 2.0F - 1.0F);
+                point.x += ship.getCollisionRadius() * ((float) Math.random() * 2f - 1f);
+                point.y += ship.getCollisionRadius() * ((float) Math.random() * 2f - 1f);
 
-                if (CollisionUtils.isPointWithinBounds(point, ship))
-                    engine.applyDamage(ship, point, amount * ship.getFluxTracker().getCurrFlux(), DamageType.OTHER, 0f,
-                        true, false, null);
+                if (!CollisionUtils.isPointWithinBounds(point, ship))
+                    point = CollisionUtils.getNearestPointOnBounds(point, ship);
+
+                engine.applyDamage(ship, point, amount * (0.20f * ship.getVariant().getHullSpec().getFluxCapacity()), DamageType.OTHER, 0f, true, false, null);
             } else {
                 stats.getMaxSpeed().unmodify();
                 stats.getMaxTurnRate().unmodify();
@@ -50,13 +54,21 @@ public class ASH_VentingOverdrive extends BaseHullMod {
     }
 
     @Override
-    public String getDescriptionParam(int index, HullSize hullSize) {
-        if (index == 0)
-            return Math.round(VENT_RATE_MODIFIER) + "";
-        if (index == 1)
-            return Math.round(MAX_SPEED_MODIFIER) + "";
-        if (index == 2)
-            return "venting hurts the ship and the higher the flux levels the greater the damage received";
-        return null;
+    public void addPostDescriptionSection(TooltipMakerAPI tooltip, HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec) {
+        float pad = 3f;
+        float opad = 10f;
+        Color b = Misc.getHighlightColor();
+        Color good = Misc.getPositiveHighlightColor();
+        Color bad = Misc.getNegativeHighlightColor();
+
+        tooltip.addSectionHeading("Effects:", Alignment.MID, opad);
+        tooltip.setBulletedListMode("");
+        tooltip.addPara("While %s and hull integrity is above %s:", opad, b, "venting", "20%");
+        tooltip.setBulletedListMode(" ^ ");
+        tooltip.addPara("Increases the rate of venting by %s.", pad, good, Math.round(SHIP_STATS_MULTIPLIER * 100f) + "%");
+        tooltip.addPara("Increases the ship's speed by %s.", pad, good, Math.round(SHIP_STATS_MULTIPLIER * 100f) + "%");
+        tooltip.addPara("Increases the ship's maneuverability by %s.", pad, good, Math.round(SHIP_STATS_MULTIPLIER * 100f) + "%");
+        tooltip.addPara("%s, the higher the flux the longer you take damage.", pad, bad, "Damages the ship");
+        tooltip.setBulletedListMode(null);
     }
 }
