8491d12a7f69486a597f2df9b21f1c8157fa9a22
unknown <james.tarrobal.777@gmail.com>
2022-10-15 19:31:57 +0800

    Update ASH_FuelRamscoop
    Update description
    Update OP cost to 3/6/9/15 from 2/4/6/8
    Update base value to 10000 from 15000
    Update tier to 2 from 3

--- src/data/hullmods/ASH_FuelRamscoop.java
+++ src/data/hullmods/ASH_FuelRamscoop.java
@@ -1,17 +1,20 @@
 package data.hullmods;
 
+import java.awt.Color;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 import com.fs.starfarer.api.fleet.FleetMemberAPI;
 import com.fs.starfarer.api.impl.hullmods.BaseLogisticsHullMod;
+import com.fs.starfarer.api.ui.Alignment;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
 
 public class ASH_FuelRamscoop extends BaseLogisticsHullMod {
     public static final float DAYS_TO_GENERATE_FUEL = 3f;
-
     private static Map<Object, Float> FUEL_TO_GENERATE = new HashMap<Object, Float>();
     static {
         FUEL_TO_GENERATE.put(HullSize.FRIGATE, 1f);
@@ -22,18 +25,6 @@ public class ASH_FuelRamscoop extends BaseLogisticsHullMod {
     private long lastDay = Global.getSector().getClock().getTimestamp();
 
     @Override
-    public String getDescriptionParam(int index, HullSize hullSize) {
-        if (index == 0)
-            return Math.round(((Float)FUEL_TO_GENERATE.get(HullSize.FRIGATE)).intValue()) + "/"
-                + Math.round(((Float)FUEL_TO_GENERATE.get(HullSize.DESTROYER)).intValue()) + "/"
-                + Math.round(((Float)FUEL_TO_GENERATE.get(HullSize.CRUISER)).intValue()) + "/"
-                + Math.round(((Float)FUEL_TO_GENERATE.get(HullSize.CAPITAL_SHIP)).intValue()) + " fuel";
-        if (index == 1)
-            return Math.round(DAYS_TO_GENERATE_FUEL) + " days";
-        return null;
-    }
-
-    @Override
     public void advanceInCampaign(FleetMemberAPI member, float amount) {
         long currentDay = Global.getSector().getClock().getTimestamp();
         float fuelGenerated = 0;
@@ -41,7 +32,7 @@ public class ASH_FuelRamscoop extends BaseLogisticsHullMod {
         if (Global.getSector().getClock().getElapsedDaysSince(lastDay) >= DAYS_TO_GENERATE_FUEL) {
             lastDay = currentDay;
 
-            if (member.getFleetData() == null || member.getFleetData().getFleet() == null)
+            if (member.getFleetData() == null || member.getFleetData().getFleet() == null || !member.getFleetData().getFleet().isPlayerFleet())
                 return;
 
             if (member.getFleetData().getFleet().getCargo() == null || member.getFleetData().getFleet().getCargo().getFuel() >= member.getFleetData().getFleet().getCargo().getMaxFuel())
@@ -49,15 +40,34 @@ public class ASH_FuelRamscoop extends BaseLogisticsHullMod {
 
             for (FleetMemberAPI fleetMember : member.getFleetData().getMembersListCopy()) {
                 if (fleetMember.getVariant().hasHullMod("ASH_FuelRamscoop"))
-                    fuelGenerated += (Float)FUEL_TO_GENERATE.get(fleetMember.getVariant().getHullSize());
+                    fuelGenerated += (Float) FUEL_TO_GENERATE.get(fleetMember.getVariant().getHullSize());
             }
 
             if (member.getFleetData().getFleet().getCargo().getFuel() + fuelGenerated >= member.getFleetData().getFleet().getCargo().getMaxFuel())
                 fuelGenerated = member.getFleetData().getFleet().getCargo().getMaxFuel() - member.getFleetData().getFleet().getCargo().getFuel();
-            if (fuelGenerated > 0)
-                Global.getSector().getCampaignUI().addMessage(Math.round(fuelGenerated) + " units of fuel has been converted using accumulated interstellar matter", Misc.getTextColor());
 
-            member.getFleetData().getFleet().getCargo().addFuel(fuelGenerated);
+            if (fuelGenerated > 0) {
+                member.getFleetData().getFleet().getCargo().addFuel(fuelGenerated);
+                Global.getSector().getCampaignUI().addMessage(Math.round(fuelGenerated) + " units of fuel has been converted using accumulated interstellar matter", Misc.getTextColor());
+            }
         }
+    }
+
+    @Override
+    public void addPostDescriptionSection(TooltipMakerAPI tooltip, HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec) {
+        float pad = 3f;
+        float opad = 10f;
+        Color b = Misc.getHighlightColor();
+        Color good = Misc.getPositiveHighlightColor();
+
+        tooltip.addSectionHeading("Effects:", Alignment.MID, opad);
+        tooltip.setBulletedListMode("");
+        tooltip.addPara("Every %s:", opad, b, "3 days");
+        tooltip.setBulletedListMode(" ^ ");
+        tooltip.addPara("Generates %s based on hull size", pad, good, Math.round(((Float) FUEL_TO_GENERATE.get(HullSize.FRIGATE)).intValue()) + "/"
+                + Math.round(((Float) FUEL_TO_GENERATE.get(HullSize.DESTROYER)).intValue()) + "/"
+                + Math.round(((Float) FUEL_TO_GENERATE.get(HullSize.CRUISER)).intValue()) + "/"
+                + Math.round(((Float) FUEL_TO_GENERATE.get(HullSize.CAPITAL_SHIP)).intValue()) + " units of fuel");
+        tooltip.setBulletedListMode(null);
     }
 }
