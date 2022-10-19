6072f5770faaad7d76172f0545387bb0ee7303cf
unknown <james.tarrobal.777@gmail.com>
2022-10-20 00:19:52 +0800

    Update hullmods
    Add ASH_AdvanceRadarSystems sprite
    Add ASH_CircuitBreakers sprite
    Remove ASH_RedundantMachinery
    Update ASH_TargetingTransceiver descriptions
    Update ASH_TargetingTransceiver OP cost to 3/6/9/15 from 4/6/10/12

--- src/data/hullmods/ASH_TargetingTransceiver.java
+++ src/data/hullmods/ASH_TargetingTransceiver.java
@@ -5,26 +5,18 @@ import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
 import com.fs.starfarer.api.impl.campaign.ids.HullMods;
+import com.fs.starfarer.api.ui.Alignment;
+import com.fs.starfarer.api.ui.TooltipMakerAPI;
+import com.fs.starfarer.api.util.Misc;
+
+import java.awt.Color;
 
 import org.lazywizard.lazylib.combat.AIUtils;
 
 public class ASH_TargetingTransceiver extends BaseHullMod {
-    public static final float WEAPON_RANGE_MODIFIER = 10f;
-    public static final float AUTOFIRE_AIM_ACCURACY_MODIFIER = 40f;
+    public static final float WEAPON_RANGE_MODIFIER = 0.10f;
+    public static final float AUTOFIRE_AIM_ACCURACY_MODIFIER = 0.40f;
     public static final float MAX_RANGE_THRESHOLD = 750f;
-
-    @Override
-    public String getDescriptionParam(int index, HullSize hullSize) {
-        if (index == 0)
-            return Math.round(WEAPON_RANGE_MODIFIER) + "%";
-        if (index == 1)
-            return Math.round(AUTOFIRE_AIM_ACCURACY_MODIFIER) + "%";
-        if (index == 2)
-            return Math.round(MAX_RANGE_THRESHOLD + 250f) + "su";
-        if (index == 3)
-            return "Targeting Core / Unit";
-        return null;
-    }
 
     @Override
     public void advanceInCombat(ShipAPI ship, float amount) {
@@ -40,17 +32,47 @@ public class ASH_TargetingTransceiver extends BaseHullMod {
                 continue;
 
             if (ally.getVariant().hasHullMod(HullMods.DEDICATED_TARGETING_CORE) ||
-                ally.getVariant().hasHullMod(HullMods.INTEGRATED_TARGETING_UNIT) ||
-                ally.getVariant().hasHullMod(HullMods.ADVANCED_TARGETING_CORE)) {
+                    ally.getVariant().hasHullMod(HullMods.INTEGRATED_TARGETING_UNIT) ||
+                    ally.getVariant().hasHullMod(HullMods.ADVANCED_TARGETING_CORE)) {
                 computedRangeBonus = WEAPON_RANGE_MODIFIER;
                 computedAutofireAimAccuracy = AUTOFIRE_AIM_ACCURACY_MODIFIER;
             }
         }
 
         if (ship.getHullSize() == HullSize.FRIGATE || ship.getHullSize() == HullSize.DESTROYER) {
-            stats.getEnergyWeaponRangeBonus().modifyPercent(spec.getId(), computedRangeBonus);
-            stats.getBallisticWeaponRangeBonus().modifyPercent(spec.getId(), computedRangeBonus);
+            stats.getEnergyWeaponRangeBonus().modifyMult(spec.getId(), 1f + computedRangeBonus);
+            stats.getBallisticWeaponRangeBonus().modifyMult(spec.getId(), 1f + computedRangeBonus);
         } else
-            stats.getAutofireAimAccuracy().modifyPercent(spec.getId(), computedAutofireAimAccuracy);
+            stats.getAutofireAimAccuracy().modifyMult(spec.getId(), 1f + computedAutofireAimAccuracy);
+    }
+
+    @Override
+    public String getDescriptionParam(int index, HullSize hullSize) {
+        if (index == 0)
+            return Math.round(WEAPON_RANGE_MODIFIER) + "%";
+        if (index == 1)
+            return Math.round(AUTOFIRE_AIM_ACCURACY_MODIFIER) + "%";
+        if (index == 2)
+            return Math.round(MAX_RANGE_THRESHOLD + 250f) + "su";
+        if (index == 3)
+            return "Targeting Core / Unit";
+        return null;
+    }
+
+    @Override
+    public void addPostDescriptionSection(TooltipMakerAPI tooltip, HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec) {
+        float pad = 3f;
+        float opad = 10f;
+        Color b = Misc.getHighlightColor();
+        Color bad = Misc.getNegativeHighlightColor();
+        Color good = Misc.getPositiveHighlightColor();
+
+        tooltip.addSectionHeading("Effects:", Alignment.MID, opad);
+        tooltip.setBulletedListMode("");
+        tooltip.addPara("If a %s has a %s and is within %s:", opad, b, "Capital or Cruise ship", "Targeting Core / Unit", Math.round(MAX_RANGE_THRESHOLD + 250f) + "su");
+        tooltip.setBulletedListMode(" ^ ");
+        tooltip.addPara("Increases the autoaim accuracy of Capital and Cruise ship's by %s", pad, good, Math.round(AUTOFIRE_AIM_ACCURACY_MODIFIER * 100f) + "%");
+        tooltip.addPara("Increases the range of non-missile weapons of destroyers and frigates by %s", pad, good, Math.round(WEAPON_RANGE_MODIFIER * 100f) + "%");
+        tooltip.setBulletedListMode(null);
     }
 }
