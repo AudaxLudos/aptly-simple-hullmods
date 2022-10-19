e47246671cb91bc8a4d68b808a56e4f712524e59
unknown <james.tarrobal.777@gmail.com>
2022-10-19 15:03:56 +0800

    Add hullmods
    Add ASH_WeaponAmplifiers
    Add ASH_WeaponLimiters
    Add ASH_WeaponOverclocks

--- src/data/hullmods/ASH_WeaponLimiters.java
+++ src/data/hullmods/ASH_WeaponLimiters.java
@@ -10,19 +10,19 @@ import com.fs.starfarer.api.ui.Alignment;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
 
-public class ASH_TestHullmod extends BaseHullMod {
-    public static final float WEAPON_DAMAGE_MULTIPLIER = 0.20f;
-    public static final float WEAPON_FLUX_MULTIPLIER = 0.10f;
+public class ASH_WeaponLimiters extends BaseHullMod {
+    public static final float WEAPON_FLUX_MULTIPLIER = 0.20f;
+    public static final float WEAPON_DAMAGE_MULTIPLIER = 0.10f;
     public static final float WEAPON_FIRE_RATE_MULTIPLIER = 0.10f;
 
     @Override
     public void applyEffectsBeforeShipCreation(HullSize hullSize, MutableShipStatsAPI stats, String id) {
-        stats.getBallisticWeaponDamageMult().modifyMult(id, 1f + WEAPON_DAMAGE_MULTIPLIER);
-        stats.getBallisticWeaponFluxCostMod().modifyMult(id, 1f + WEAPON_FLUX_MULTIPLIER);
+        stats.getBallisticWeaponFluxCostMod().modifyMult(id, 1f + -WEAPON_FLUX_MULTIPLIER);
+        stats.getBallisticWeaponDamageMult().modifyMult(id, 1f + -WEAPON_DAMAGE_MULTIPLIER);
         stats.getBallisticRoFMult().modifyMult(id, 1f + -WEAPON_FIRE_RATE_MULTIPLIER);
 
-        stats.getEnergyWeaponDamageMult().modifyMult(id, 1f + WEAPON_DAMAGE_MULTIPLIER);
-        stats.getEnergyWeaponFluxCostMod().modifyMult(id, 1f + WEAPON_FLUX_MULTIPLIER);
+        stats.getEnergyWeaponFluxCostMod().modifyMult(id, 1f + -WEAPON_FLUX_MULTIPLIER);
+        stats.getEnergyWeaponDamageMult().modifyMult(id, 1f + -WEAPON_DAMAGE_MULTIPLIER);
         stats.getEnergyRoFMult().modifyMult(id, 1f + -WEAPON_FIRE_RATE_MULTIPLIER);
     }
 
@@ -35,8 +35,8 @@ public class ASH_TestHullmod extends BaseHullMod {
 
         tooltip.addSectionHeading("Effects:", Alignment.MID, opad);
         tooltip.setBulletedListMode(" - ");
-        tooltip.addPara("Increases the damage of non-missile weapons by %s", opad, good, Math.round(WEAPON_DAMAGE_MULTIPLIER * 100f) + "%");
-        tooltip.addPara("Increases the flux cost of non-missile weapons by %s", pad, bad, Math.round(WEAPON_FLUX_MULTIPLIER * 100f) + "%");
+        tooltip.addPara("Decreases the flux cost of non-missile weapons by %s", opad, good, Math.round(WEAPON_FLUX_MULTIPLIER * 100f) + "%");
+        tooltip.addPara("Decreases the damage of non-missile weapons by %s", pad, bad, Math.round(WEAPON_DAMAGE_MULTIPLIER * 100f) + "%");
         tooltip.addPara("Decreases the fire rate of non-missile weapons by %s", pad, bad, Math.round(WEAPON_FIRE_RATE_MULTIPLIER * 100f) + "%");
         tooltip.setBulletedListMode(null);
     }
