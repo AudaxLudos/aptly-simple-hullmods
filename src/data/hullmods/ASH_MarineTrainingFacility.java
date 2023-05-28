a166514ac2a019bce0d6a002dd4c24b48f9b64fd
unknown <james.tarrobal.777@gmail.com>
2023-05-28 22:49:21 +0800

    Fix marin training facility crash when getting accessed outside of combat

--- src/data/hullmods/ASH_MarineTrainingFacility.java
+++ src/data/hullmods/ASH_MarineTrainingFacility.java
@@ -4,11 +4,8 @@ import java.awt.Color;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
-import com.fs.starfarer.api.fleet.FleetMemberAPI;
-import com.fs.starfarer.api.impl.PlayerFleetPersonnelTracker;
 import com.fs.starfarer.api.impl.hullmods.BaseLogisticsHullMod;
 import com.fs.starfarer.api.ui.Alignment;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
@@ -17,73 +14,22 @@ import com.fs.starfarer.api.util.Misc;
 public class ASH_MarineTrainingFacility extends BaseLogisticsHullMod {
     public static final int DAYS_TO_GENERATE_MARINES = 7;
     public static final int MARINES_TO_TRAIN_MULTIPLIER = 20;
-    private static Map<Object, Integer> MARINES_TO_GENERATE = new HashMap<Object, Integer>();
+    public static Map<Object, Integer> MARINES_TO_GENERATE = new HashMap<Object, Integer>();
     static {
         MARINES_TO_GENERATE.put(HullSize.FRIGATE, 10);
         MARINES_TO_GENERATE.put(HullSize.DESTROYER, 20);
         MARINES_TO_GENERATE.put(HullSize.CRUISER, 30);
         MARINES_TO_GENERATE.put(HullSize.CAPITAL_SHIP, 50);
     }
-    private static Map<Object, Integer> MAX_MARINES_TO_GENERATE = new HashMap<Object, Integer>();
+    public static Map<Object, Integer> MAX_MARINES_TO_GENERATE = new HashMap<Object, Integer>();
     static {
         MAX_MARINES_TO_GENERATE.put(HullSize.FRIGATE, 50);
         MAX_MARINES_TO_GENERATE.put(HullSize.DESTROYER, 100);
         MAX_MARINES_TO_GENERATE.put(HullSize.CRUISER, 150);
         MAX_MARINES_TO_GENERATE.put(HullSize.CAPITAL_SHIP, 250);
     }
-    private long lastDay = Global.getSector().getClock().getTimestamp();
 
-    @Override
-    public void advanceInCampaign(FleetMemberAPI member, float amount) {
-        long currentDay = Global.getSector().getClock().getTimestamp();
-        int maxMarines = 0;
-        int addMarines = 0;
-        float trainMarines = 0;
-
-        if (Global.getSector().getClock().getElapsedDaysSince(lastDay) >= DAYS_TO_GENERATE_MARINES) {
-            lastDay = currentDay;
-
-            if (member.getFleetData() == null || member.getFleetData().getFleet() == null || !member.getFleetData().getFleet().isPlayerFleet())
-                return;
-
-            if (member.getFleetData().getFleet().getCargo() == null || member.getFleetData().getFleet().getCargo().getCrew() <= member.getFleetData().getMinCrew())
-                return;
-
-            for (FleetMemberAPI fleetMember : member.getFleetData().getMembersListCopy()) {
-                if (fleetMember.getVariant().hasHullMod("ASH_MarineTrainingFacility")) {
-                    addMarines += (Integer) MARINES_TO_GENERATE.get(fleetMember.getVariant().getHullSize());
-                    maxMarines += (Integer) MAX_MARINES_TO_GENERATE.get(fleetMember.getVariant().getHullSize());
-                    trainMarines += (Integer) MARINES_TO_GENERATE.get(fleetMember.getVariant().getHullSize()) * (MARINES_TO_TRAIN_MULTIPLIER / 100f);
-                }
-            }
-
-            if (PlayerFleetPersonnelTracker.getInstance().getMarineData().getXPLevel() * member.getFleetData().getFleet().getCargo().getMarines() + trainMarines >= member.getFleetData().getFleet()
-                    .getCargo().getMarines())
-                trainMarines = member.getFleetData().getFleet().getCargo().getMarines() - PlayerFleetPersonnelTracker.getInstance().getMarineData().getXPLevel() *
-                        member.getFleetData().getFleet().getCargo().getMarines();
-
-            if (trainMarines > 0 && member.getFleetData().getFleet().getCargo().getMarines() > 0) {
-                Global.getSector().getCampaignUI().addMessage(Math.round(trainMarines) + " existing marines has been trained.", Misc.getTextColor());
-                PlayerFleetPersonnelTracker.getInstance().getMarineData().addXP(Math.round(trainMarines));
-                PlayerFleetPersonnelTracker.getInstance().update();
-            }
-
-            if (member.getFleetData().getFleet().getCargo().getMarines() >= maxMarines)
-                return;
-
-            if (member.getFleetData().getFleet().getCargo().getMarines() + addMarines >= maxMarines)
-                addMarines = maxMarines - (int) member.getFleetData().getFleet().getCargo().getMarines();
-
-            if (member.getFleetData().getFleet().getCargo().getCrew() - addMarines <= member.getFleetData().getMinCrew())
-                addMarines = (int) (member.getFleetData().getFleet().getCargo().getCrew() - member.getFleetData().getMinCrew());
-
-            if (addMarines > 0) {
-                member.getFleetData().getFleet().getCargo().addMarines(addMarines);
-                member.getFleetData().getFleet().getCargo().removeCrew(addMarines);
-                Global.getSector().getCampaignUI().addMessage(addMarines + " crew has been trained into marines.", Misc.getTextColor());
-            }
-        }
-    }
+    // Affects are done in ASH_MarineTrainingFacilityScript.java
 
     @Override
     public void addPostDescriptionSection(TooltipMakerAPI tooltip, HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec) {
@@ -96,10 +42,11 @@ public class ASH_MarineTrainingFacility extends BaseLogisticsHullMod {
         tooltip.setBulletedListMode("");
         tooltip.addPara("Every %s:", opad, b, "week");
         tooltip.setBulletedListMode(" ^ ");
-        tooltip.addPara("levels up %s current marines based on hull size", pad, good, Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.FRIGATE)).intValue()) * (MARINES_TO_TRAIN_MULTIPLIER / 100f) + "/"
-                + Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.DESTROYER)).intValue()) * (MARINES_TO_TRAIN_MULTIPLIER / 100f) + "/"
-                + Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.CRUISER)).intValue()) * (MARINES_TO_TRAIN_MULTIPLIER / 100f) + "/"
-                + Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.CAPITAL_SHIP)).intValue()) * (MARINES_TO_TRAIN_MULTIPLIER / 100f));
+        tooltip.addPara("levels up %s current marines based on hull size", pad, good,
+                Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.FRIGATE)).intValue() * (MARINES_TO_TRAIN_MULTIPLIER / 100f)) + "/"
+                        + Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.DESTROYER)).intValue() * (MARINES_TO_TRAIN_MULTIPLIER / 100f)) + "/"
+                        + Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.CRUISER)).intValue() * (MARINES_TO_TRAIN_MULTIPLIER / 100f)) + "/"
+                        + Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.CAPITAL_SHIP)).intValue() * (MARINES_TO_TRAIN_MULTIPLIER / 100f)));
         tooltip.addPara("Converts %s crew members into marines based on hull size", pad, good, Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.FRIGATE)).intValue()) + "/"
                 + Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.DESTROYER)).intValue()) + "/"
                 + Math.round(((Integer) MARINES_TO_GENERATE.get(HullSize.CRUISER)).intValue()) + "/"
---
+++ src/data/scripts/campaign/ASH_MarineTrainingFacilityScript.java
@@ -0,0 +1,98 @@
+package data.scripts.campaign;
+
+import java.util.List;
+
+import com.fs.starfarer.api.EveryFrameScript;
+import com.fs.starfarer.api.Global;
+import com.fs.starfarer.api.campaign.CampaignFleetAPI;
+import com.fs.starfarer.api.campaign.CargoAPI;
+import com.fs.starfarer.api.campaign.FleetDataAPI;
+import com.fs.starfarer.api.fleet.FleetMemberAPI;
+import com.fs.starfarer.api.impl.PlayerFleetPersonnelTracker;
+import com.fs.starfarer.api.util.Misc;
+
+import data.hullmods.ASH_MarineTrainingFacility;
+
+public class ASH_MarineTrainingFacilityScript implements EveryFrameScript {
+    public boolean isActive = false;
+    public Long lastDay;
+
+    @Override
+    public void advance(float amount) {
+        if (Global.getSector().getPlayerFleet() == null)
+            return;
+        if (Global.getSector().getPlayerFleet().getFleetData() == null)
+            return;
+        if (Global.getSector().getPlayerFleet().getCargo() == null)
+            return;
+
+        CampaignFleetAPI playerFleet = Global.getSector().getPlayerFleet();
+        FleetDataAPI playerFleetData = playerFleet.getFleetData();
+        CargoAPI playerCargo = playerFleet.getCargo();
+        List<FleetMemberAPI> playerFleetMembers = playerFleet.getFleetData().getMembersListCopy();
+
+        if (!isActive) {
+            for (FleetMemberAPI fleetMember : playerFleetMembers) {
+                if (fleetMember.getVariant().hasHullMod("ASH_MarineTrainingFacility")) {
+                    isActive = true;
+                    lastDay = Global.getSector().getClock().getTimestamp();
+                    break;
+                }
+            }
+        } else {
+            int maxMarines = 0;
+            int addMarines = 0;
+            float trainMarines = 0;
+
+            System.out.println(Global.getSector().getClock().getElapsedDaysSince(lastDay));
+            if (Global.getSector().getClock().getElapsedDaysSince(lastDay) >= ASH_MarineTrainingFacility.DAYS_TO_GENERATE_MARINES) {
+                for (FleetMemberAPI fleetMember : playerFleetMembers) {
+                    if (fleetMember.getVariant().hasHullMod("ASH_MarineTrainingFacility")) {
+                        addMarines += (Integer) ASH_MarineTrainingFacility.MARINES_TO_GENERATE.get(fleetMember.getVariant().getHullSize());
+                        maxMarines += (Integer) ASH_MarineTrainingFacility.MAX_MARINES_TO_GENERATE.get(fleetMember.getVariant().getHullSize());
+                        trainMarines += (Integer) ASH_MarineTrainingFacility.MARINES_TO_GENERATE.get(fleetMember.getVariant().getHullSize())
+                                * (ASH_MarineTrainingFacility.MARINES_TO_TRAIN_MULTIPLIER / 100f);
+                    }
+                }
+
+                if (PlayerFleetPersonnelTracker.getInstance().getMarineData().getXPLevel() * playerCargo.getMarines() + trainMarines >= playerCargo.getMarines())
+                    trainMarines = playerCargo.getMarines() - PlayerFleetPersonnelTracker.getInstance().getMarineData().getXPLevel() *
+                            playerCargo.getMarines();
+
+                if (trainMarines > 0 && playerCargo.getMarines() > 0) {
+                    PlayerFleetPersonnelTracker.getInstance().getMarineData().addXP(Math.round(trainMarines));
+                    PlayerFleetPersonnelTracker.getInstance().update();
+                    Global.getSector().getCampaignUI().addMessage(Math.round(trainMarines) + " existing marines has been trained.", Misc.getTextColor());
+                }
+
+                if (playerCargo.getMarines() + addMarines >= maxMarines)
+                    addMarines = maxMarines - (int) playerCargo.getMarines();
+
+                if (playerCargo.getCrew() - addMarines <= playerFleetData.getMinCrew())
+                    addMarines = (int) (playerCargo.getCrew() - playerFleetData.getMinCrew());
+
+                if (playerCargo.getCrew() <= playerFleetData.getMinCrew() || playerCargo.getMarines() >= maxMarines)
+                    addMarines = 0;
+
+                if (addMarines > 0) {
+                    playerCargo.addMarines(addMarines);
+                    playerCargo.removeCrew(addMarines);
+                    Global.getSector().getCampaignUI().addMessage(addMarines + " crew has been trained into marines.", Misc.getTextColor());
+                }
+
+                isActive = false;
+            }
+        }
+    }
+
+    @Override
+    public boolean isDone() {
+        return false;
+    }
+
+    @Override
+    public boolean runWhilePaused() {
+        return false;
+    }
+
+}
