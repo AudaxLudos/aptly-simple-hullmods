013b2cb70c9c20cd716729fba0040d705378cbc9
AudaxLudos <james.tarrobal.777@gmail.com>
2023-10-25 20:46:03 +0800

    Update ASH_FluxConductionArmor flux capacity increase to 20% from 15% Update ASH_FluxConductionArmor energy damage taken reduce to 10% from 15% Add ASH_FluxConductionArmor armor bonus decrease of 10% from 0% Update ASH_FluxConductionArmor smod bonus to fully negate armor bonus penalty

--- src/data/hullmods/ASH_FluxConductionArmor.java
+++ src/data/hullmods/ASH_FluxConductionArmor.java
@@ -10,21 +10,18 @@ import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
 
 public class ASH_FluxConductionArmor extends BaseHullMod {
-    public static final float FLUX_CAPACITY_MULTIPLIER = 0.15f;
-    public static final float ENERGY_DAMAGE_TAKEN_MULTIPLIER = 0.15f;
+    public static final float FLUX_CAPACITY_MULT = 0.20f;
+    public static final float ENERGY_DAMAGE_TAKEN_MULT = 0.10f;
+    public static final float ARMOR_BONUS_MULT = 0.10f;
 
     @Override
     public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
-        float fluxCapacityMult = FLUX_CAPACITY_MULTIPLIER;
-        float energyDamageTakenMult = ENERGY_DAMAGE_TAKEN_MULTIPLIER;
-
+        stats.getFluxCapacity().modifyMult(id, 1f + FLUX_CAPACITY_MULT);
+        stats.getEnergyDamageTakenMult().modifyMult(id, 1f + ENERGY_DAMAGE_TAKEN_MULT);
+        stats.getArmorBonus().modifyMult(id, 1f - ARMOR_BONUS_MULT);
         if (isSMod(stats)) {
-            fluxCapacityMult += 0.05f;
-            energyDamageTakenMult -= 0.05f;
+            stats.getArmorBonus().unmodify(id);
         }
-
-        stats.getFluxCapacity().modifyPercent(id, 100f * fluxCapacityMult);
-        stats.getEnergyDamageTakenMult().modifyMult(id, 1f + energyDamageTakenMult);
     }
 
     @Override
@@ -35,21 +32,18 @@ public class ASH_FluxConductionArmor extends BaseHullMod {
         Color bad = Misc.getNegativeHighlightColor();
 
         tooltip.setBulletedListMode(" - ");
-        tooltip.addPara("Increases the ship's flux capacity by %s", opad, good, Math.round(FLUX_CAPACITY_MULTIPLIER * 100f) + "%");
-        tooltip.addPara("Increases the energy damage taken by %s", pad, bad, Math.round(ENERGY_DAMAGE_TAKEN_MULTIPLIER * 100f) + "%");
+        tooltip.addPara("Increases the ship's flux capacity by %s", opad, good, Math.round(FLUX_CAPACITY_MULT * 100f) + "%");
+        tooltip.addPara("Increases the energy damage taken by %s", pad, bad, Math.round(ENERGY_DAMAGE_TAKEN_MULT * 100f) + "%");
+        tooltip.addPara("Decreases the armor bonus of the ship by %s", pad, bad, Math.round(ARMOR_BONUS_MULT * 100f) + "%");
         tooltip.setBulletedListMode(null);
     }
 
     @Override
     public void addSModEffectSection(TooltipMakerAPI tooltip, ShipAPI.HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec, boolean isForBuildInList) {
         float pad = 3f;
-        float opad = 10f;
-        Color good = Misc.getPositiveHighlightColor();
-        Color bad = Misc.getNegativeHighlightColor();
 
         tooltip.setBulletedListMode(" - ");
-        tooltip.addPara("Increases the ship's flux capacity by an additional %s", opad, good, Math.round(5f) + "%");
-        tooltip.addPara("Reduces the energy damage taken penalty by %s", pad, bad, Math.round(5f) + "%");
+        tooltip.addPara("Fully negates the armor bonus penalty", pad);
         tooltip.setBulletedListMode(null);
     }
 
