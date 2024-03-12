9f826d588d2b4bdd6ae51cc0ef9990aa34cd40ef
AudaxLudos <james.tarrobal.777@gmail.com>
2024-03-13 05:29:44 +0800

    Update reduce deflective shields kinetic damage taken to 10% from 20% Update Add deflective shields energy damage taken of 10% Update reduce shock absorbers explosive damage taken to 10% from 20% Update Add shock absorbers fragmentation damage taken of 10%

--- src/aptlysimplehullmods/hullmods/DeflectiveShields.java
+++ src/aptlysimplehullmods/hullmods/DeflectiveShields.java
@@ -9,24 +9,27 @@ import com.fs.starfarer.api.util.Misc;
 import java.awt.*;
 
 public class DeflectiveShields extends BaseHullMod {
-    public static final float KINETIC_DMG_TAKEN_MULT = 0.20f;
+    public static final float DMG_TAKEN_MULT = 0.10f;
     public static final float EXPLOSIVE_DMG_TAKEN_MULT = 0.20f;
 
     @Override
     public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
-        stats.getKineticShieldDamageTakenMult().modifyMult(id, 1f - KINETIC_DMG_TAKEN_MULT);
+        stats.getKineticShieldDamageTakenMult().modifyMult(id, 1f - DMG_TAKEN_MULT);
+        stats.getEnergyShieldDamageTakenMult().modifyMult(id, 1f - DMG_TAKEN_MULT);
         stats.getHighExplosiveShieldDamageTakenMult().modifyMult(id, 1f + EXPLOSIVE_DMG_TAKEN_MULT);
     }
 
     @Override
     public void addPostDescriptionSection(TooltipMakerAPI tooltip, ShipAPI.HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec) {
         float oPad = 10f;
+        float pad = 5f;
         Color good = Misc.getPositiveHighlightColor();
         Color bad = Misc.getNegativeHighlightColor();
 
         tooltip.setBulletedListMode(" - ");
-        tooltip.addPara("Decreases the kinetic damage taken of shields by %s", oPad, bad, Math.round(KINETIC_DMG_TAKEN_MULT * 100f) + "%");
-        tooltip.addPara("Increases the explosive damage taken of shields by %s", oPad, good, Math.round(EXPLOSIVE_DMG_TAKEN_MULT * 100f) + "%");
+        tooltip.addPara("Decreases the kinetic damage taken of a shield by %s", oPad, good, Math.round(DMG_TAKEN_MULT * 100f) + "%");
+        tooltip.addPara("Decreases the energy damage taken of a shield by %s", pad, good, Math.round(DMG_TAKEN_MULT * 100f) + "%");
+        tooltip.addPara("Increases the explosive damage taken of a shield by %s", pad, bad, Math.round(EXPLOSIVE_DMG_TAKEN_MULT * 100f) + "%");
         tooltip.setBulletedListMode(null);
     }
 }
--- src/aptlysimplehullmods/hullmods/ShockAbsorbers.java
+++ src/aptlysimplehullmods/hullmods/ShockAbsorbers.java
@@ -9,24 +9,27 @@ import com.fs.starfarer.api.util.Misc;
 import java.awt.*;
 
 public class ShockAbsorbers extends BaseHullMod {
-    public static final float EXPLOSIVE_DMG_TAKEN_MULT = 0.20f;
+    public static final float DMG_TAKEN_MULT = 0.10f;
     public static final float KINETIC_DMG_TAKEN_MULT = 0.20f;
 
     @Override
     public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
-        stats.getHighExplosiveDamageTakenMult().modifyMult(id, 1f - EXPLOSIVE_DMG_TAKEN_MULT);
+        stats.getHighExplosiveDamageTakenMult().modifyMult(id, 1f - DMG_TAKEN_MULT);
+        stats.getFragmentationDamageTakenMult().modifyMult(id, 1f - DMG_TAKEN_MULT);
         stats.getKineticDamageTakenMult().modifyMult(id, 1f + KINETIC_DMG_TAKEN_MULT);
     }
 
     @Override
     public void addPostDescriptionSection(TooltipMakerAPI tooltip, ShipAPI.HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec) {
         float oPad = 10f;
+        float pad = 5f;
         Color good = Misc.getPositiveHighlightColor();
         Color bad = Misc.getNegativeHighlightColor();
 
         tooltip.setBulletedListMode(" - ");
-        tooltip.addPara("Decreases the explosive damage taken of the ship by %s", oPad, good, Math.round(EXPLOSIVE_DMG_TAKEN_MULT * 100f) + "%");
-        tooltip.addPara("Increases the kinetic damage taken of the ship by %s", oPad, bad, Math.round(KINETIC_DMG_TAKEN_MULT * 100f) + "%");
+        tooltip.addPara("Decreases the explosive damage taken of a ship by %s", oPad, good, Math.round(DMG_TAKEN_MULT * 100f) + "%");
+        tooltip.addPara("Decreases the fragmentation damage taken of a ship by %s", pad, good, Math.round(DMG_TAKEN_MULT * 100f) + "%");
+        tooltip.addPara("Increases the kinetic damage taken of a ship by %s", pad, bad, Math.round(KINETIC_DMG_TAKEN_MULT * 100f) + "%");
         tooltip.setBulletedListMode(null);
     }
 }
