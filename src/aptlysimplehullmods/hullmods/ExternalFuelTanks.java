e1e05f2ec3642d85b2ac54fe316da3616f1bf028
AudaxLudos <james.tarrobal.777@gmail.com>
2024-03-13 15:56:28 +0800

    Update external fuel tanks description

--- src/aptlysimplehullmods/hullmods/ExternalFuelTanks.java
+++ src/aptlysimplehullmods/hullmods/ExternalFuelTanks.java
@@ -39,11 +39,11 @@ public class ExternalFuelTanks extends BaseLogisticsHullMod {
         Color bad = Misc.getNegativeHighlightColor();
 
         tooltip.setBulletedListMode(" - ");
-        tooltip.addPara("Increases the fuel capacity by %s based on hull size", oPad, good, FUEL_MODIFIER.get(HullSize.FRIGATE).intValue() + "/"
+        tooltip.addPara("Increases the ship's fuel capacity by %s based on hull size", oPad, good, FUEL_MODIFIER.get(HullSize.FRIGATE).intValue() + "/"
                 + FUEL_MODIFIER.get(HullSize.DESTROYER).intValue() + "/"
                 + FUEL_MODIFIER.get(HullSize.CRUISER).intValue() + "/"
                 + FUEL_MODIFIER.get(HullSize.CAPITAL_SHIP).intValue() + " points");
-        tooltip.addPara("Reduces the ship's flux dissipation by %s", pad, bad, Math.round(SHIP_STATS_MULTIPLIER * 100f) + "%");
+        tooltip.addPara("Decreases the ship's flux dissipation by %s", pad, bad, Math.round(SHIP_STATS_MULTIPLIER * 100f) + "%");
         tooltip.addPara("Increases the ship's sensor profile by %s", pad, bad, Math.round(SHIP_STATS_MULTIPLIER * 100f) + "%");
         tooltip.addPara("Increases the explosive damage taken by %s", pad, bad, Math.round(SHIP_STATS_MULTIPLIER * 100f) + "%");
         tooltip.setBulletedListMode(null);
