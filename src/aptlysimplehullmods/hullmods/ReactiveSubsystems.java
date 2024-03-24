11b6059f75e2a9d10e9d52e58badbd2c24203084
AudaxLudos <james.tarrobal.777@gmail.com>
2024-03-24 23:23:04 +0800

    Remove ReactiveSubsystems cr degredation rate penalty Remove ReactiveSubsystems peak performance smod bonus Add ReactiveSubsystems deployment points penalty of 1/2/3/4 Update ReactiveSubsystems descriptions

--- src/aptlysimplehullmods/hullmods/ReactiveSubsystems.java
+++ src/aptlysimplehullmods/hullmods/ReactiveSubsystems.java
@@ -4,23 +4,32 @@ import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.MutableShipStatsAPI;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
+import com.fs.starfarer.api.impl.campaign.ids.Stats;
 import com.fs.starfarer.api.ui.TooltipMakerAPI;
 import com.fs.starfarer.api.util.Misc;
 
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ReactiveSubsystems extends BaseHullMod {
     public static final float MAX_CR_MOD = 0.15f;
-    public static final float CR_LOSS_PER_SECOND_MULT = 0.25f;
-    public static final float PEAK_CR_DURATION_MOD = 60f;
+    public static Map<HullSize, Float> DEPLOYMENT_POINTS_MOD = new HashMap<>();
+
+    static {
+        DEPLOYMENT_POINTS_MOD.put(HullSize.FRIGATE, 1f);
+        DEPLOYMENT_POINTS_MOD.put(HullSize.DESTROYER, 2f);
+        DEPLOYMENT_POINTS_MOD.put(HullSize.CRUISER, 3f);
+        DEPLOYMENT_POINTS_MOD.put(HullSize.CAPITAL_SHIP, 4f);
+    }
 
     @Override
     public void applyEffectsBeforeShipCreation(ShipAPI.HullSize hullSize, MutableShipStatsAPI stats, String id) {
         stats.getMaxCombatReadiness().modifyFlat(id, MAX_CR_MOD, "Reactive Subsystems Hullmod");
-        stats.getCRLossPerSecondPercent().modifyMult(id, 1f + CR_LOSS_PER_SECOND_MULT);
+        stats.getDynamic().getMod(Stats.DEPLOYMENT_POINTS_MOD).modifyFlat(id, DEPLOYMENT_POINTS_MOD.get(hullSize));
 
         if (isSMod(stats))
-            stats.getPeakCRDuration().modifyFlat(id, PEAK_CR_DURATION_MOD);
+            stats.getDynamic().getMod(Stats.DEPLOYMENT_POINTS_MOD).unmodify(id);
     }
 
     @Override
@@ -32,17 +41,20 @@ public class ReactiveSubsystems extends BaseHullMod {
 
         tooltip.setBulletedListMode(" - ");
         tooltip.addPara("Increases the ship's max combat readiness by %s", oPad, good, Math.round(MAX_CR_MOD * 100f) + "%");
-        tooltip.addPara("Increases the ship's rate at which combat readiness degrades by %s", pad, bad, Math.round(CR_LOSS_PER_SECOND_MULT * 100f) + "%");
+        tooltip.addPara("Increases the ship's deployment points by %s based on its hull size", pad, bad, DEPLOYMENT_POINTS_MOD.get(HullSize.FRIGATE).intValue() + "/"
+                + DEPLOYMENT_POINTS_MOD.get(HullSize.DESTROYER).intValue() + "/"
+                + DEPLOYMENT_POINTS_MOD.get(HullSize.CRUISER).intValue() + "/"
+                + DEPLOYMENT_POINTS_MOD.get(HullSize.CAPITAL_SHIP).intValue());
         tooltip.setBulletedListMode(null);
     }
 
     @Override
     public void addSModEffectSection(TooltipMakerAPI tooltip, ShipAPI.HullSize hullSize, ShipAPI ship, float width, boolean isForModSpec, boolean isForBuildInList) {
         float oPad = 10f;
-        Color good = Misc.getPositiveHighlightColor();
+        Color b = Misc.getHighlightColor();
 
         tooltip.setBulletedListMode(" - ");
-        tooltip.addPara("Increases the ship's peak performance time by %s", oPad, good, Math.round(PEAK_CR_DURATION_MOD) + " seconds");
+        tooltip.addPara("Fully negates the ship's deployment points penalty", oPad, b);
         tooltip.setBulletedListMode(null);
     }
 
