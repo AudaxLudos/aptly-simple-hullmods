ad6b1cd12c9346c5855cd248aadb1a0b084643c8
AudaxLudos <james.tarrobal.777@gmail.com>
2024-04-23 22:29:01 +0800

    Update front loaded armor hullmod code structure

--- src/aptlysimplehullmods/hullmods/FrontLoadedArmor.java
+++ src/aptlysimplehullmods/hullmods/FrontLoadedArmor.java
@@ -14,35 +14,25 @@ public class FrontLoadedArmor extends BaseHullMod {
     public static final float NEGATIVE_ARMOR_VALUE_MULT = 0.5f;
 
     @Override
-    public void advanceInCombat(ShipAPI ship, float amount) {
-        if (!ship.isAlive())
-            return;
+    public void applyEffectsAfterShipCreation(ShipAPI ship, String id) {
+        if (ship == null) return;
 
         float[][] armorGrid = ship.getArmorGrid().getGrid();
         int armorCellX = armorGrid.length;
         int armorCellY = armorGrid[0].length;
-        boolean initArmor = false;
 
-        if (Global.getCombatEngine().getCustomData().get("ASH_FrontLoadedArmor_" + ship.getId()) instanceof Boolean)
-            initArmor = (boolean) Global.getCombatEngine().getCustomData().get("ASH_FrontLoadedArmor_" + ship.getId());
-
-        if (!initArmor) {
-            initArmor = true;
-            for (int x = 0; x < armorCellX; x++) {
-                for (int y = 0; y < armorCellY; y++) {
-                    float currentArmorValue = ship.getArmorGrid().getArmorValue(x, y);
-                    if (y >= armorCellY / 2f) {
-                        ship.getArmorGrid().setArmorValue(x, y, currentArmorValue * POSITIVE_ARMOR_VALUE_MULT);
-                    } else {
-                        if (isSMod(ship))
-                            continue;
-                        ship.getArmorGrid().setArmorValue(x, y, currentArmorValue * NEGATIVE_ARMOR_VALUE_MULT);
-                    }
+        for (int x = 0; x < armorCellX; x++) {
+            for (int y = 0; y < armorCellY; y++) {
+                float currentArmorValue = ship.getArmorGrid().getArmorValue(x, y);
+                if (y >= armorCellY / 2f) {
+                    ship.getArmorGrid().setArmorValue(x, y, currentArmorValue * POSITIVE_ARMOR_VALUE_MULT);
+                } else {
+                    if (isSMod(ship))
+                        continue;
+                    ship.getArmorGrid().setArmorValue(x, y, currentArmorValue * NEGATIVE_ARMOR_VALUE_MULT);
                 }
             }
         }
-
-        Global.getCombatEngine().getCustomData().put("ASH_FrontLoadedArmor_" + ship.getId(), initArmor);
     }
 
     @Override
