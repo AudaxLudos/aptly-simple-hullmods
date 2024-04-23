4e6347ad602b80549982520fcd45399d03be86c6
AudaxLudos <james.tarrobal.777@gmail.com>
2024-04-23 23:10:14 +0800

    Update hidden compact shields hullmo

--- src/aptlysimplehullmods/hullmods/FrontLoadedArmor.java
+++ src/aptlysimplehullmods/hullmods/FrontLoadedArmor.java
@@ -1,6 +1,5 @@
 package aptlysimplehullmods.hullmods;
 
-import com.fs.starfarer.api.Global;
 import com.fs.starfarer.api.combat.BaseHullMod;
 import com.fs.starfarer.api.combat.ShipAPI;
 import com.fs.starfarer.api.combat.ShipAPI.HullSize;
