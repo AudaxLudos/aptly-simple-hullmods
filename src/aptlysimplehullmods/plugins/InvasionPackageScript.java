21a1a773fcb822ad5fddbdfafbb6cde0b09e9e15
AudaxLudos <james.tarrobal.777@gmail.com>
2025-01-21 22:59:16 +0800

    Update code format

--- src/aptlysimplehullmods/plugins/InvasionPackageScript.java
+++ src/aptlysimplehullmods/plugins/InvasionPackageScript.java
@@ -10,8 +10,6 @@ import com.fs.starfarer.api.impl.campaign.ids.Stats;
 
 public class InvasionPackageScript implements EveryFrameScript {
     public static final String INVASION_PACKAGE_ID = "ash_invasion_package";
-    public boolean isActive = false;
-    public int shipsWithHullmod = 0;
 
     @Override
     public boolean isDone() {
@@ -33,44 +31,35 @@ public class InvasionPackageScript implements EveryFrameScript {
         }
 
         CampaignFleetAPI playerFleet = Global.getSector().getPlayerFleet();
-        Float bonus = (Float) playerFleet.getFleetData().getCacheClearedOnSync().get(INVASION_PACKAGE_ID);
-        if (bonus != null) {
+        Float bonusStat = (Float) playerFleet.getFleetData().getCacheClearedOnSync().get(INVASION_PACKAGE_ID);
+        if (bonusStat != null) {
             return;
         }
 
-        int shipsWithInvasionPackage = 0;
-        float totalStat = 0;
+        int shipsWithMod = 0;
+        bonusStat = 0f;
         for (FleetMemberAPI member : playerFleet.getFleetData().getMembersListCopy()) {
             if (member.isMothballed()) {
                 continue;
             }
             if (member.getVariant().hasHullMod(INVASION_PACKAGE_ID)) {
-                ++shipsWithInvasionPackage;
-                totalStat += InvasionPackage.PLANETARY_OPERATION_CASUALTIES_MULT.get(member.getVariant().getHullSize());
+                ++shipsWithMod;
+                bonusStat += InvasionPackage.PLANETARY_OPERATION_CASUALTIES_MULT.get(member.getVariant().getHullSize());
             }
         }
 
-        if (this.shipsWithHullmod != shipsWithInvasionPackage) {
-            // un-modify fleet stats here
-            this.isActive = false;
-            this.shipsWithHullmod = shipsWithInvasionPackage;
+        if (bonusStat > 0f) {
+            playerFleet.getStats().getDynamic().getMod(Stats.PLANETARY_OPERATIONS_MOD).modifyMult(INVASION_PACKAGE_ID,
+                    1f + Utils.computeStatMultiplier(bonusStat),
+                    shipsWithMod + " ships with Invasion Package");
+            playerFleet.getStats().getDynamic().getStat(Stats.PLANETARY_OPERATIONS_CASUALTIES_MULT).modifyMult(INVASION_PACKAGE_ID,
+                    1f - Utils.computeStatMultiplier(bonusStat),
+                    shipsWithMod + " ships with Invasion Package");
+        } else {
+            playerFleet.getStats().getDynamic().getMod(Stats.PLANETARY_OPERATIONS_MOD).unmodify(INVASION_PACKAGE_ID);
+            playerFleet.getStats().getDynamic().getStat(Stats.PLANETARY_OPERATIONS_CASUALTIES_MULT).unmodify(INVASION_PACKAGE_ID);
         }
 
-        if (!this.isActive) {
-            // modify fleet stats here
-            this.isActive = true;
-            if (totalStat > 0f) {
-                playerFleet.getStats().getDynamic().getMod(Stats.PLANETARY_OPERATIONS_MOD).modifyMult(INVASION_PACKAGE_ID,
-                        1f + Utils.computeStatMultiplier(totalStat),
-                        this.shipsWithHullmod + " ships with Invasion Package");
-                playerFleet.getStats().getDynamic().getStat(Stats.PLANETARY_OPERATIONS_CASUALTIES_MULT).modifyMult(INVASION_PACKAGE_ID,
-                        1f - Utils.computeStatMultiplier(totalStat),
-                        this.shipsWithHullmod + " ships with Invasion Package");
-            } else {
-                playerFleet.getStats().getDynamic().getMod(Stats.PLANETARY_OPERATIONS_MOD).unmodify(INVASION_PACKAGE_ID);
-                playerFleet.getStats().getDynamic().getStat(Stats.PLANETARY_OPERATIONS_CASUALTIES_MULT).unmodify(INVASION_PACKAGE_ID);
-            }
-        }
-        playerFleet.getFleetData().getCacheClearedOnSync().put(INVASION_PACKAGE_ID, totalStat);
+        playerFleet.getFleetData().getCacheClearedOnSync().put(INVASION_PACKAGE_ID, bonusStat);
     }
 }
